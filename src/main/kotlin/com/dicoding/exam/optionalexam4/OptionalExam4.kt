5f8807c37489231e56bd5816d6ad701cfcefd947
Ushaim Ramadhan F <adhan1005@gmail.com>
2025-12-30 18:51:38 +0700

    feat: finish coding in the OptionalExam4.kt

--- src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
@@ -1,6 +1,14 @@
 package com.dicoding.exam.optionalexam4
 
-// TODO
 fun getMiddleCharacters(string: String): String {
-    return ""
+    require(string.length in 1..100) { "string must contain 1-100 characters" }
+
+    val len = string.length
+    val mid = len / 2
+
+    return if (len % 2 == 0) {
+        string.substring(mid - 1, mid + 1)
+    } else {
+        string[mid].toString()
+    }
 }
