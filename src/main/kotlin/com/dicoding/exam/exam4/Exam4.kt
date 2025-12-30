a3cae5126c44cd6a3ba9ce2fe189968091133a3b
Ushaim Ramadhan F <adhan1005@gmail.com>
2025-12-30 16:41:43 +0700

    feat: finish coding in the Exam4.kt

--- src/main/kotlin/com/dicoding/exam/exam4/Exam4.kt
+++ src/main/kotlin/com/dicoding/exam/exam4/Exam4.kt
@@ -1,11 +1,16 @@
-//package com.dicoding.exam.exam4
-//
-//// TODO
-//open class Car() {
-//    open fun getCarInfo(): String
-//}
-//
-//// TODO
-//class ElectricCar() : Car() {
-//    override fun getCarInfo(): String
-//}
+package com.dicoding.exam.exam4
+
+// TODO
+open class Car(val brand: String, val year: Int) {
+    open fun getCarInfo(): String {
+        return "$brand keluaran tahun $year"
+
+    }
+}
+
+// TODO
+class ElectricCar(pBrand: String, pYear: Int, val batteryCapacity: Int) : Car(pBrand, pYear) {
+    override fun getCarInfo(): String {
+        return "$brand keluaran tahun $year dengan kapasitas baterai $batteryCapacity kwh"
+    }
+}
