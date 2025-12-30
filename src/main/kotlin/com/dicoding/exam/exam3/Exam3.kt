eed8a6ed7d10ffba2f8d2316d2f505df270a7b1a
Ushaim Ramadhan F <adhan1005@gmail.com>
2025-12-30 16:01:03 +0700

    feat: finish coding in the Exam3.kt

--- src/main/kotlin/com/dicoding/exam/exam3/Exam3.kt
+++ src/main/kotlin/com/dicoding/exam/exam3/Exam3.kt
@@ -1,4 +1,8 @@
 package com.dicoding.exam.exam3
 
 // TODO
-fun vehicle() = Any()
+fun vehicle() : Map<String, String> = mapOf(
+    "type" to "motorcycle",
+    "maxSpeed" to "230Km/s",
+    "maxTank" to "100Ltr"
+)
