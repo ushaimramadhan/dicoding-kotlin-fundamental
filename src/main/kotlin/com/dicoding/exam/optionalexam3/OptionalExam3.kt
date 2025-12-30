3bdd8ea027414b6e97c916d02abe7a10a882c3ef
Ushaim Ramadhan F <adhan1005@gmail.com>
2025-12-30 18:38:38 +0700

    feat: finish coding in the OptionalExam3.kt

--- src/main/kotlin/com/dicoding/exam/optionalexam3/OptionalExam3.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam3/OptionalExam3.kt
@@ -2,5 +2,17 @@ package com.dicoding.exam.optionalexam3
 
 // TODO
 fun manipulateString(str: String, int: Int): String {
-    return ""
+    require(int in 1..100) { "int must be in 1..100" }
+
+    val regex = "\\d+".toRegex()
+    val match = regex.find(str)
+
+    return if (match != null) {
+        val numberPart = match.value.toInt()
+        val multiplied = numberPart * int
+        val textPart = str.substring(0, match.range.first)
+        textPart + multiplied
+    } else {
+        str + int
+    }
 }
