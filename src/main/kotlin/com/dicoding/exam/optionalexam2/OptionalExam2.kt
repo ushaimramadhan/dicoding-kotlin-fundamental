3941c6086befc519e7af918925f3a2cd8939b366
Ushaim Ramadhan F <adhan1005@gmail.com>
2025-12-30 18:30:05 +0700

    feat: finish coding in the OptionalExam2.kt

--- src/main/kotlin/com/dicoding/exam/optionalexam2/OptionalExam2.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam2/OptionalExam2.kt
@@ -2,5 +2,12 @@ package com.dicoding.exam.optionalexam2
 
 // TODO
 fun minAndMax(number: Int): Int {
-    return 0
+    val str = kotlin.math.abs(number).toString()
+    require(str.length in 2..9) { "Panjang digit harus 2 sampai 9"}
+
+    val digits = str.map { it - '0' }
+
+    val minDigit = digits.minOrNull()!!
+    val maxDigit = digits.maxOrNull()!!
+    return minDigit + maxDigit
 }
